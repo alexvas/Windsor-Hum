ed12019706882983b1440e805369c34f53a7424e
a.a.vasiljev <a.a.vasiljev+git@gmail.com>
2011-08-17 21:43:06 +0700

    make yellow yellow

--- src/hum/client/LevelHelper.java
+++ src/hum/client/LevelHelper.java
@@ -7,7 +7,7 @@ import com.google.inject.Singleton;
 
 @Singleton
 public class LevelHelper {
-    private final IconBuilder yellow = new IconBuilder().primaryColor("00ffff");
+    private final IconBuilder yellow = new IconBuilder().primaryColor("ffff00");
     private final IconBuilder green = new IconBuilder().primaryColor("00ff00");
     private final IconBuilder red = new IconBuilder().primaryColor("ff0000");
 
